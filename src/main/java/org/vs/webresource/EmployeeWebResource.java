bee073892ea6f758c2088bffd1f5ac1b7aeff90d
Vishal Sinha <vishal.sinha21@gmail.com>
2015-02-21 19:45:44 +0100

    add liquibase as dependency as well, add data to employee table

--- src/main/java/org/vs/webresource/EmployeeWebResource.java
+++ src/main/java/org/vs/webresource/EmployeeWebResource.java
@@ -1,6 +1,7 @@
 package org.vs.webresource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class EmployeeWebResource {
     }
 
     @RequestMapping(value = "/employee/{employeeId}", method = RequestMethod.GET)
-    public Employee getEmployeeById(String employeeId) {
+    public Employee getEmployeeById(@PathVariable String employeeId) {
         return employeeService.getEmployee(new BigInteger(employeeId));
     }
 
